853dbde823d68c36c5b35c8c635e916d1ad044ac
[lamthanhbinh] <[binh@bliss-interactive.net]>
2014-09-27 23:27:17 +0700

    contact page

--- wp-content/themes/musician/tpl-contact.php
+++ wp-content/themes/musician/tpl-contact.php
@@ -1,96 +1,108 @@
-<section id="contact" style="background-color: #fff; color: #133939; padding-top: 0px; padding-bottom: 0px;">
-    <div id="map" data-latitude="40.78717" data-longitude="-73.943524" class="map-canvas ss-effect" data-ss-effect="fade-from-bottom" data-ss-effect-speed="1" data-ss-effect-delay="0" data-ss-effect-offset="2">
+<section id="contact" class="contact" style="background: url(images/contact/contact_bg.jpg) no-repeat center center; background-size: cover; color: #fff; padding-top: 0px; padding-bottom: 0px;">
+    <div class="bgdot">
+    	
     </div>
     <div class="container">
         <div class="row">
             <div class="col-sm-12">
-                <div class="section-header" style="padding-top: 60px;">
+                <div class="section-header" style="padding-top: 100px;">
                     <div class="section-header-inner">
-                        <div class="section-heading-left-line ss-effect" data-ss-effect="fade-from-bottom" data-ss-effect-speed="1" data-ss-effect-delay="0" data-ss-effect-offset="2"></div>
+                        <div class="section-heading-left-tick ss-effect" data-ss-effect="fade-from-bottom" data-ss-effect-speed="1" data-ss-effect-delay="0" data-ss-effect-offset="2"></div>
                         <h1 class="section-heading ss-effect" data-ss-effect="fade-from-top" data-ss-effect-speed="1" data-ss-effect-delay="0.2" data-ss-effect-offset="2">
                             <span>CONTACT US</span>
                         </h1>
-                        <div class="section-heading-right-line ss-effect" data-ss-effect="fade-from-bottom" data-ss-effect-speed="1" data-ss-effect-delay="0" data-ss-effect-offset="2"></div>
+                        <div class="section-heading-right-tick ss-effect" data-ss-effect="fade-from-bottom" data-ss-effect-speed="1" data-ss-effect-delay="0" data-ss-effect-offset="2"></div>
                     </div>
                 </div>
             </div>
         </div>
+       <div class="container">
+		        <div class="row">
+		            <div class="col-sm-12">
+		
+		                <div class="section-content ss-typography">
+		
+		                    <div class="row">
+		                        <div class="contact-container contact-1">
+		                            
+		                            <div class="col-sm-6 col-6-1 ss-effect" data-ss-effect="fade-from-right" data-ss-effect-speed="1" data-ss-effect-delay="0.2" data-ss-effect-offset="2">
+		                                <div class="contact-info alignRight">
+		                                    <h3>CONTACT INFO</h3>
+		                                    <div class="box-icon-container">
+		                                       
+		                                        <div class="box-icon-content small">
+		                                            SVEN FAULCONER
+		                                        </div>
+		                                    </div>
+		                                    <div class="gap" data-height-size="25px"></div>
+		                                    <div class="box-icon-container">
+		                                        <div class="box-icon-content small">
+		                                            4216 MCCONNELL BLVD,<br/>
+													CULVER CITY, CA 90066,<br/>
+													UNITED STATES
+		                                        </div>
+		                                    </div>
+		                                    <div class="gap" data-height-size="25px"></div>
+		                                    <div class="box-icon-container">
+		                                        <div class="box-icon-content small">
+		                                          T. 310-463-3744<br/>
+		                                          <a href="mailto:sven@falconscore.com">sven@falconscore.com</a>
+		                                        </div>
+		                                    </div>
+		                                </div>
+		                            </div>
+		                            
+		                            <div class="col-sm-6  col-6-2 ss-effect" data-ss-effect="fade-from-left" data-ss-effect-speed="1" data-ss-effect-delay="0.2" data-ss-effect-offset="2">
+		                                <div id="contact-respond" class="contact-respond">
+		                                    <h3 id="reply-title" class="contact-reply-title">SEND ME A MESSAGE</h3>
+		                                    <form action="php/contact.php" method="post" id="contactform" class="contact-form" novalidate="novalidate">
+		                                        <div id="respond-inputs" class="respond-inputs">
+		                                            <div class="row-input">
+		                                            	<div class="col-title paddingT5">
+			                                            	Name
+			                                            </div>
+			                                            <div class="col-ip">
+			                                            	<input id="contact-name" name="contact-name" type="text" placeholder="NAME" aria-required="true" />
+			                                            </div>
+		                                            </div>
+		                                            <div class="row-input ">
+			                                             <div class="col-title paddingT10">
+			                                            	Email
+			                                            </div>
+			                                            <div class="col-ip">
+			                                            	<input id="contact-email" name="contact-email" type="email" placeholder="EMAIL" aria-required="true" />
+			                                            </div>
+		                                            </div>
+		                                            <div class="row-input">
+		                                            	<div class="col-title paddingT5">
+			                                            	Message
+			                                            </div>
+			                                            <div class="col-ip">
+			                                            	<div id="contact-text" class="contact-text">
+					                                            <textarea id="contact-message" name="contact-message" cols="45" rows="10" aria-required="true" placeholder="MESSAGE"></textarea>
+					                                        </div>
+					                                    </div>
+		                                            </div>
+		                                        </div>
+		                                        
+		                                        <p class="form-submit">
+		                                            <img class="ajax-loader" src="images/contact/ajax-loader.gif" alt="Sending ..." style="visibility: hidden;" />
+		                                            <input class="submit" name="submit" type="submit" id="submit" value="SEND" />
+		                                        </p>
+		                                        <div class="contactform-response-output contactform-display-none"></div>
+		                                    </form>
+		                                </div>
+		                            </div>
+		                        </div>
+		
+		                    </div>
+		
+		                </div>
+		
+		            </div>
+		        </div>
+		
+		    </div>
     </div>
-</section>
-<section class="section-content-container" style="padding-bottom: 90px; padding-top: 0px; margin-top: 0;">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12">
-
-                <div class="section-content ss-typography">
-
-                    <div class="row">
-                        <div class="contact-container contact-1">
-                            <div class="col-sm-8 ss-effect" data-ss-effect="fade-from-left" data-ss-effect-speed="1" data-ss-effect-delay="0.2" data-ss-effect-offset="2">
-                                <h2>Get in Touch with Us</h2>
-                                <p>
-                                    Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vivamus suscipit tortor eget felis porttitor volutpat. Donec rutrum congue leo eget malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque velit nisi, pretium ut lacinia in, 
-                                </p>
-                                <div id="contact-respond" class="contact-respond">
-                                    <h3 id="reply-title" class="contact-reply-title">CONTACT FORM</h3>
-                                    <form action="php/contact.php" method="post" id="contactform" class="contact-form" novalidate="novalidate">
-                                        <div id="respond-inputs" class="respond-inputs">
-                                            <p>
-                                                <input id="contact-name" name="contact-name" type="text" placeholder="NAME" aria-required="true" />
-                                            </p>
-                                            <p>
-                                                <input id="contact-email" name="contact-email" type="email" placeholder="EMAIL" aria-required="true" />
-                                            </p>
-                                            <p class="last">
-                                                <input id="contact-url" name="contact-url" type="text" placeholder="WEBSITE" />
-                                            </p>
-                                        </div>
-                                        <div id="contact-text" class="contact-text">
-                                            <textarea id="contact-message" name="contact-message" cols="45" rows="10" aria-required="true" placeholder="MESSAGE"></textarea>
-                                        </div>
-                                        <p class="form-submit">
-                                            <img class="ajax-loader" src="images/contact/ajax-loader.gif" alt="Sending ..." style="visibility: hidden;" />
-                                            <input class="submit" name="submit" type="submit" id="submit" value="SEND MESSAGE" />
-                                        </p>
-                                        <div class="contactform-response-output contactform-display-none"></div>
-                                    </form>
-                                </div>
-                            </div>
-                            <div class="col-sm-4 ss-effect" data-ss-effect="fade-from-right" data-ss-effect-speed="1" data-ss-effect-delay="0.2" data-ss-effect-offset="2">
-                                <div class="contact-info">
-                                    <h3>CONTACT INFO</h3>
-                                    <div class="box-icon-container">
-                                        <span aria-hidden="true" class="icon-location nivan-icon small"></span>
-                                        <div class="box-icon-content small">
-                                            1600 AMPHITHEATRE PARKWAY, 
-                                            MOUNTAIN VIEW, CA 94043, 
-                                            UNITED STATES
-                                        </div>
-                                    </div>
-                                    <div class="gap" data-height-size="30px"></div>
-                                    <div class="box-icon-container">
-                                        <span aria-hidden="true" class="icon-phone4 nivan-icon small"></span>
-                                        <div class="box-icon-content small">
-                                            +321 123 456 7
-                                        </div>
-                                    </div>
-                                    <div class="gap" data-height-size="10px"></div>
-                                    <div class="box-icon-container">
-                                        <span aria-hidden="true" class="icon-envelope3 nivan-icon small"></span>
-                                        <div class="box-icon-content small">
-                                            bonjour@spnoy.com
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </div>
-        </div>
-
-    </div>
+    
 </section>
