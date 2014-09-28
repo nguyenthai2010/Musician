0cb89ca3afbd3660b4da2bd0d09d9ff2eaefca05
Thai Nguyen <thai@bliss-interactive.net>
2014-09-28 23:40:24 +0700

    .

--- wp-content/themes/musician/tpl-music.php
+++ wp-content/themes/musician/tpl-music.php
@@ -1,3 +1,9 @@
+<?php
+	$args_music = array(
+		'post_type' 	 => 'music'
+	);
+	$query_music = get_posts($args_music);
+?>
 <section id="music" class="music">
     <div class="container">
         <div class="row">
@@ -14,22 +20,20 @@
 
                 <div class="section-content ss-typography">
                     <div class="portfolio-button-group center-align ss-effect" data-ss-effect="fade-from-bottom" data-ss-effect-speed="1" data-ss-effect-delay="0.2" data-ss-effect-offset="2">
-                        <label class="radio-input-checked">
-                            <input type="radio" name="portfolio-radios" value="*" checked>FAVORITES</label>
-                        <label>
-                            <input type="radio" name="portfolio-radios" value=".branding">ACTION</label>
-                        <label>
-                            <input type="radio" name="portfolio-radios" value=".logo">ELECTRONIC</label>
-                        <label>
-                            <input type="radio" name="portfolio-radios" value=".video">DRAMA</label>
-                        <label >
-                            <input type="radio" name="portfolio-radios" value="*" checked>FAVORITES</label>
-                        <label>
-                            <input type="radio" name="portfolio-radios" value=".branding">ACTION</label>
-                        <label>
-                            <input type="radio" name="portfolio-radios" value=".logo">ELECTRONIC</label>
-                        <label>
-                            <input type="radio" name="portfolio-radios" value=".video">DRAMA</label>
+<?php 
+$terms = get_terms( 'music-tax' );
+//print_r($terms);
+foreach ( $terms as $term ) {
+    $term_link = get_term_link( $term );
+    if ( is_wp_error( $term_link ) ) {
+        continue;
+    }
+
+	echo '<label><input type="radio" name="portfolio-radios" value=".branding">' . $term->name . ' </label>';
+}
+?>
+                    
+                        <!--<label class="radio-input-checked"><input type="radio" name="portfolio-radios" value="*" checked>FAVORITES</label>-->
                     </div>
                     <div class="music-list center-align ss-effect" data-ss-effect="fade-from-bottom" data-ss-effect-speed="1" data-ss-effect-delay="0.2" data-ss-effect-offset="2">
                     	<div class="row-audio top">
@@ -38,38 +42,22 @@
                             <div class="time">TIME</div>
                             <div class="id">ID</div>
                         </div>
-                        
-                        <div class="row-audio active">
-                        	<div class="no">1</div>
-                            <div class="title">The first song</div>
-                            <div class="time">0:47</div>
-                            <div class="id">TGS05</div>
-                        </div>
+
+						<?php
+							$number = 0;
+                            foreach ( $query_music as $music ) {
+                                $number ++;
+                                $id = get_post_meta($music->ID, '_cmb__musis_id_text', true);
+								$file = get_post_meta($music->ID, '_cmb__musis_path_file', true);
+                                
+                        ?>
                         <div class="row-audio">
-                        	<div class="no">1</div>
-                            <div class="title">The first song The first songThe first songThe first songThe first songThe first songThe first songThe first song</div>
-                            <div class="time">0:47</div>
-                            <div class="id">TGS05</div>
+                        	<div class="no"><?php echo $number?></div>
+                            <div class="title"><?php echo $music->post_title;?></div>
+                            <div class="time"><?php echo $metadata['length']?></div>
+                            <div class="id"><?php echo $id?></div>
                         </div>
-                        <div class="row-audio">
-                        	<div class="no">1</div>
-                            <div class="title">The first song</div>
-                            <div class="time">0:47</div>
-                            <div class="id">TGS05</div>
-                        </div>
-                        <div class="row-audio">
-                        	<div class="no">1</div>
-                            <div class="title">The first song</div>
-                            <div class="time">0:47</div>
-                            <div class="id">TGS05</div>
-                        </div>
-                        <div class="row-audio">
-                        	<div class="no">1</div>
-                            <div class="title">The first song</div>
-                            <div class="time">0:47</div>
-                            <div class="id">TGS05</div>
-                        </div>
-                        
+                        <?php }?>
                     </div>
                 </div>
         </div>
