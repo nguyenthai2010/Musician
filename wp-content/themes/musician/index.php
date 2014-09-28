d0374f110f6d69d61ef7d6b1ef0718b25868e932
Thai Nguyen <thai@bliss-interactive.net>
2014-09-28 14:49:34 +0700

    .

--- wp-content/themes/musician/index.php
+++ wp-content/themes/musician/index.php
@@ -6,22 +6,11 @@
         <!-- Services Section-->
         <?php get_template_part('tpl','bio');?>
 
-        <!-- Parallax Section -->
-        <?php //get_template_part('tpl','parallax');?>
-
-        <!-- About Section -->
-        <?php get_template_part('tpl','about');?>   
-
         <!-- Portfolio Section-->
         <?php get_template_part('tpl','credits');?>   
-        
-        <!-- testimonial Section -->
-        <?php //get_template_part('tpl','testimonial');?>   
-        
 
-        <!-- Blog Section -->
-        <?php get_template_part('tpl','blog');?>   
-        
+        <!-- Music Section-->
+        <?php get_template_part('tpl','music');?>   
 
         <!-- Contact Section -->
         <?php get_template_part('tpl','contact');?>
---
+++ wp-content/themes/musician/tpl-music.php
@@ -0,0 +1,77 @@
+<section id="music" class="music">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="section-header">
+                    <div class="section-header-inner">
+                        <div class="section-heading-left-tick ss-effect" data-ss-effect="fade-from-bottom" data-ss-effect-speed="1" data-ss-effect-delay="0" data-ss-effect-offset="2"></div>
+                        <h1 class="section-heading ss-effect" data-ss-effect="fade-from-top" data-ss-effect-speed="1" data-ss-effect-delay="0.2" data-ss-effect-offset="2">
+                            <span>Music</span>
+                        </h1>
+                        <div class="section-heading-right-tick ss-effect" data-ss-effect="fade-from-bottom" data-ss-effect-speed="1" data-ss-effect-delay="0" data-ss-effect-offset="2"></div>
+                    </div>
+                </div>
+
+                <div class="section-content ss-typography">
+                    <div class="portfolio-button-group center-align ss-effect" data-ss-effect="fade-from-bottom" data-ss-effect-speed="1" data-ss-effect-delay="0.2" data-ss-effect-offset="2">
+                        <label class="radio-input-checked">
+                            <input type="radio" name="portfolio-radios" value="*" checked>FAVORITES</label>
+                        <label>
+                            <input type="radio" name="portfolio-radios" value=".branding">ACTION</label>
+                        <label>
+                            <input type="radio" name="portfolio-radios" value=".logo">ELECTRONIC</label>
+                        <label>
+                            <input type="radio" name="portfolio-radios" value=".video">DRAMA</label>
+                        <label >
+                            <input type="radio" name="portfolio-radios" value="*" checked>FAVORITES</label>
+                        <label>
+                            <input type="radio" name="portfolio-radios" value=".branding">ACTION</label>
+                        <label>
+                            <input type="radio" name="portfolio-radios" value=".logo">ELECTRONIC</label>
+                        <label>
+                            <input type="radio" name="portfolio-radios" value=".video">DRAMA</label>
+                    </div>
+                    <div class="music-list center-align ss-effect" data-ss-effect="fade-from-bottom" data-ss-effect-speed="1" data-ss-effect-delay="0.2" data-ss-effect-offset="2">
+                    	<div class="row-audio top">
+                        	<div class="no">#</div>
+                            <div class="title">TITLE</div>
+                            <div class="time">TIME</div>
+                            <div class="id">ID</div>
+                        </div>
+                        
+                        <div class="row-audio">
+                        	<div class="no">1</div>
+                            <div class="title">The first song</div>
+                            <div class="time">0:47</div>
+                            <div class="id">TGS05</div>
+                        </div>
+                        <div class="row-audio">
+                        	<div class="no">1</div>
+                            <div class="title">The first song The first songThe first songThe first songThe first songThe first songThe first songThe first song</div>
+                            <div class="time">0:47</div>
+                            <div class="id">TGS05</div>
+                        </div>
+                        <div class="row-audio">
+                        	<div class="no">1</div>
+                            <div class="title">The first song</div>
+                            <div class="time">0:47</div>
+                            <div class="id">TGS05</div>
+                        </div>
+                        <div class="row-audio">
+                        	<div class="no">1</div>
+                            <div class="title">The first song</div>
+                            <div class="time">0:47</div>
+                            <div class="id">TGS05</div>
+                        </div>
+                        <div class="row-audio">
+                        	<div class="no">1</div>
+                            <div class="title">The first song</div>
+                            <div class="time">0:47</div>
+                            <div class="id">TGS05</div>
+                        </div>
+                        
+                    </div>
+                </div>
+        </div>
+    </div>
+</section>
