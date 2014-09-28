1d07e898c9c826acfcb09c1cf11b7335df67f8b2
Thai Nguyen <thai@bliss-interactive.net>
2014-09-28 13:53:40 +0700

    music admin

--- wp-content/themes/musician/meta-box/custom.php
+++ wp-content/themes/musician/meta-box/custom.php
@@ -1,24 +1,29 @@
 <?php
-function add_metaboxes_page( $meta_boxes ) {
+function add_metaboxes_music( $meta_boxes ) {
     $prefix = '_cmb_'; // Prefix for all fields
-    $meta_boxes['page_description'] = array(
-        'id' => 'page_description',
-        'title' => 'Description',
-        'pages' => array('page'), // post type
+    $meta_boxes['music_metabox'] = array(
+        'id' => 'music_metabox',
+        'title' => 'Information of Music',
+        'pages' => array('music'), // post type
         'context' => 'normal',
         'priority' => 'high',
         'show_names' => false, // Show field names on the left
         'fields' => array(
             array(
-                'name' => '',
-                'desc' => '',
-                'id' => $prefix . 'page_description_text',
-                'type' => 'wysiwyg'
+				'name' => 'Music ID',
+                'desc' => 'Music ID',
+                'id'   => $prefix . '_musis_id_text',
+                'type' => 'text'
             ),
+			array(
+				'name' => 'Music File',
+                'desc' => 'Music File',
+                'id'   => $prefix . '_musis_path_file',
+                'type' => 'file'
+            )
         ),
     );
 
     return $meta_boxes;
 }
-//add_filter( 'cmb_meta_boxes', 'add_metaboxes_properties' );
-
+add_filter( 'cmb_meta_boxes', 'add_metaboxes_music' );
