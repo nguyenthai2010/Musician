9e809466b1447740e2753ad40181d52aab9c55a5
[lamthanhbinh] <[binh@bliss-interactive.net]>
2014-10-06 21:22:07 +0700

    update css

--- wp-content/themes/musician/tpl-home.php
+++ wp-content/themes/musician/tpl-home.php
@@ -31,7 +31,7 @@
 							$url = wp_get_attachment_url( get_post_thumbnail_id($slider->ID) );
 							$selling = get_post_meta($slider->ID, '_cmb_selling_slider', true);
                     ?>
-                    <li data-transition="fade" data-slotamount="8">
+                    <li data-transition="fade" data-slotamount="8" >
                         <img src="<?php echo $url;?>" alt="" />
                         <div class="caption fade start" data-x="0" data-y="0" data-start="0" data-speed="500" style="background-color: rgba(0, 0, 0, 0); background-image: url(images/banner-shadow1.png); background-size: cover; width: 100%; height: 100%; position: absolute; background-position: 50% 50%; background-repeat: no-repeat no-repeat;"></div>
                     </li>
