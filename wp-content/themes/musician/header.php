a9bfe2c4c72009fdbaa048a843cc7cfa9513de73
Thai Nguyen <thai@bliss-interactive.net>
2014-10-20 15:40:46 +0700

    .

--- wp-content/themes/musician/header.php
+++ wp-content/themes/musician/header.php
@@ -6,8 +6,7 @@
     <!-- Meta Tags -->
     <meta charset="UTF-8">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta name="description" content="Nivan One Page/Multi Page HTML Template">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="description" content="">
     <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"/><![endif]-->
 
     <!-- Title -->
